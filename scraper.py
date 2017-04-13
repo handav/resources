040341e5c3d84cfd1373d6c501704d320557b81c
handav <hannahdavis0220@gmail.com>
2017-04-13 16:01:31 +0200

    generalizing scraper

--- scraper.py
+++ scraper.py
@@ -6,31 +6,27 @@ import requests
 
 csvfolder = './agg'
 imagefolder = './images/'
-counter = {}
 
 def getUrls(filepath):
     with open(filepath, 'rb') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            emotion = row[0]
+            #change name to whatever you want it
+            name = row[0]
+            #make sure url is the correct row, row[1] refers to a specific dataset
             url = row[1]
-            no = int(row[2])
-            yes = int(row[3])
-            saveto = imagefolder + emotion + '/'
-            if yes > 3:
-                downloadPhoto(url, saveto, emotion)
+            #if you want to put them in different folders, add that after 'imagefolder'
+            saveto = imagefolder + '/'
+            downloadPhoto(url, saveto, name)
 
 
-def downloadPhoto(url, filepath, emotion):
+def downloadPhoto(url, filepath, name):
     if (os.path.isdir(filepath) == False):
         print 'no folder called '+filepath
         os.makedirs(filepath)
 
-    if emotion not in counter:
-        counter[emotion] = 0
-
     response = requests.get(url, stream=True)
-    filename = filepath + str(counter[emotion])
+    filename = filepath + name
     if not (os.path.exists(filename)):
         print 'saving '+filename
         with open(filename, 'wb') as out_file:
@@ -38,12 +34,10 @@ def downloadPhoto(url, filepath, emotion):
         del response
     else:
         print filename + ' already exists'
-    counter[emotion]+=1
-
 
 
 for filename in os.listdir(csvfolder):
-    if filename.endswith(".csv") and 'amusement' not in filename and 'anger' not in filename and 'awe' not in filename and 'contentment' not in filename and 'disgust' not in filename and 'excitement' not in filename and 'fear' not in filename: 
+    if filename.endswith(".csv"): 
         getUrls(os.path.join(csvfolder, filename))
         continue
     else:
